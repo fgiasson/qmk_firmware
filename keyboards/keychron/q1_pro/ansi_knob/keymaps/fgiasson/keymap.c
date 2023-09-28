da1a6ff630c6248db48399d36c030e42c03959fa
Frederick Giasson <fred@fgiasson.com>
2023-09-28 19:24:58 -0400

    Early version of the Q1 Pro keymap for the Keychron Q1 Pro keyboard.

---
+++ keyboards/keychron/q1_pro/ansi_knob/keymaps/fgiasson/keymap.c
@@ -0,0 +1,176 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+// Some useless macros to eventually delete
+enum custom_keycodes {
+    VIM_SEL_QUOTE = SAFE_RANGE + 100,
+    VIM_SEL_QUOTE_I = SAFE_RANGE + 101
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case VIM_SEL_QUOTE:
+            if (record->event.pressed) {
+                SEND_STRING("l"); // Move one space to the right, this is to make sure that we can do it multiple times in a row
+                SEND_STRING("/'");
+                SEND_STRING(SS_TAP(X_ENTER));
+                SEND_STRING("lvt'd");
+            }
+            break;
+        case VIM_SEL_QUOTE_I:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_ESC)); // make sure we are in NORMAL mode
+                SEND_STRING("l"); // Move one space to the right, this is to make sure that we can do it multiple times in a row
+                SEND_STRING("/'");
+                SEND_STRING(SS_TAP(X_ENTER));
+                SEND_STRING("lvt'di");
+            }
+            break;
+    }
+    return true;
+};
+
+
+// Some type dances for the party
+// Tap Dance declarations
+enum {
+    TD_DOT_GT,
+    TD_COMMA_LT,
+    TD_SEMICOLON_COLON,
+    TD_SLASH_QM,
+    TD_QUOTE_DQUOTE,
+    TD_BACKSLASH_PIPE,
+    TD_LBRC_LCBR,
+    TD_RBRC_RCBR,
+    TD_PLUS_EQUAL,
+    TD_MINUS_UNDERSCORE,
+    TD_ZERO_CLOSEPAR,
+    TD_NINE_OPENPAR,
+    TD_EIGHT_ASTERISK,
+    TD_SEVEN_AMPERSAND,
+    TD_SIX_CARET,
+    TD_FIVE_PERCENT,
+    TD_FOUR_DOLLAR,
+    TD_THREE_HASH,
+    TD_TWO_AT,
+    TD_ONE_EXCLAMATION,
+    TD_BACKTICK_TILDE,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_DOT_GT] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_GT),
+    [TD_COMMA_LT] = ACTION_TAP_DANCE_DOUBLE(KC_COMMA, KC_LT),
+    [TD_SEMICOLON_COLON] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
+    [TD_SLASH_QM] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_QUESTION),
+    [TD_QUOTE_DQUOTE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
+    [TD_BACKSLASH_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE),
+    [TD_LBRC_LCBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
+    [TD_RBRC_RCBR] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
+    [TD_PLUS_EQUAL] = ACTION_TAP_DANCE_DOUBLE(KC_PLUS, KC_EQUAL),
+    [TD_MINUS_UNDERSCORE] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDS),
+    [TD_ZERO_CLOSEPAR] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_RIGHT_PAREN),
+    [TD_NINE_OPENPAR] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_LEFT_PAREN),
+    [TD_EIGHT_ASTERISK] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_ASTERISK),
+    [TD_SEVEN_AMPERSAND] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_AMPERSAND),
+    [TD_SIX_CARET] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_CIRCUMFLEX),
+    [TD_FIVE_PERCENT] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_PERCENT),
+    [TD_FOUR_DOLLAR] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_DOLLAR),
+    [TD_THREE_HASH] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_HASH),
+    [TD_TWO_AT] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_AT),
+    [TD_ONE_EXCLAMATION] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_EXCLAIM),
+    [TD_BACKTICK_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_GRAVE, KC_TILDE),
+};
+
+// clang-format off
+enum layers{
+  MAC_BASE,
+  MAC_FN,
+  WIN_BASE,
+  WIN_FN,
+  VIM_COMMAND,
+  VIM_INSERT
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ [MAC_BASE] = LAYOUT_ansi_82(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_DEL,             KC_MUTE,
+        TD(TD_BACKTICK_TILDE),   TD(TD_ONE_EXCLAMATION),     TD(TD_TWO_AT),     TD(TD_THREE_HASH),     TD(TD_FOUR_DOLLAR),     TD(TD_FIVE_PERCENT),     TD(TD_SIX_CARET),     TD(TD_SEVEN_AMPERSAND),     TD(TD_EIGHT_ASTERISK),     TD(TD_NINE_OPENPAR),     TD(TD_ZERO_CLOSEPAR),     TD(TD_MINUS_UNDERSCORE),  TD(TD_PLUS_EQUAL),   KC_BACKSPACE,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     TD(TD_LBRC_LCBR),  TD(TD_RBRC_RCBR),  TD(TD_BACKSLASH_PIPE),            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     TD(TD_SEMICOLON_COLON),  TD(TD_QUOTE_DQUOTE),            KC_ENT,             KC_HOME,
+        SC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     TD(TD_COMMA_LT),  TD(TD_DOT_GT),   TD(TD_SLASH_QM),            SC_RSPC,  KC_UP,
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 MO(VIM_COMMAND),MO(MAC_FN),MO(VIM_INSERT),  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [MAC_FN] = LAYOUT_ansi_82(
+        KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,            RGB_TOG,
+        KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_PLY2,  DM_REC2,            KC_TRNS,            KC_END,
+        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  DM_PLY1,  DM_REC1,            DM_RSTP,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+    [WIN_BASE] = LAYOUT_ansi_82(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+        SC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            SC_RSPC,  KC_UP,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                  MO(VIM_COMMAND), MO(WIN_FN),MO(VIM_INSERT),  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [WIN_FN] = LAYOUT_ansi_82(
+        KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,            RGB_TOG,
+        KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_END,
+        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+    [VIM_COMMAND] = LAYOUT_ansi_82(
+        KC_ESC,   KC_C,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            KC_MUTE,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+        XXXXXXX,  VIM_SEL_QUOTE,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,                               KC_SPC,                                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX),
+
+    [VIM_INSERT] = LAYOUT_ansi_82(
+        KC_ESC,   KC_I,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            RGB_TOG,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+        XXXXXXX,  VIM_SEL_QUOTE_I,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,                               KC_SPC,                                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX)
+
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [MAC_FN] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [WIN_FN] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [VIM_COMMAND] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [VIM_INSERT] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
+};
+#endif // ENCODER_MAP_ENABLE
+
+
+
+// this is a '', and this is another 'test'
+
---
+++ keyboards/keychron/q1_pro/ansi_knob/keymaps/fgiasson/rules.mk
@@ -0,0 +1,12 @@
+# Enable more layers than the default which is 4
+OPT_DEFS += -DDYNAMIC_KEYMAP_LAYER_COUNT=7
+
+# Ref: https://docs.qmk.fm/#/feature_space_cadet
+# disable it such that it doesn't conflict with Space Cadet shifts behaviours
+COMMAND_ENABLE = no
+
+# Ref: https://docs.qmk.fm/#/feature_dynamic_macros
+DYNAMIC_MACRO_ENABLE = yes
+
+# Ref: https://docs.qmk.fm/#/feature_tap_dance
+TAP_DANCE_ENABLE = yes
