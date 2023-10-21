1d3937396026fb79cacb57b231d129f6fdee0a54
Frederick Giasson <fred@fgiasson.com>
2023-10-21 14:01:27 -0400

    Remove double taps

--- keyboards/keychron/q1_pro/ansi_knob/keymaps/fgiasson/keymap.c
+++ keyboards/keychron/q1_pro/ansi_knob/keymaps/fgiasson/keymap.c
@@ -31,6 +31,7 @@ enum custom_keycodes {
     VIM_SPLIT_WINDOW_VERTICALLY = SAFE_RANGE + 109,
     VIM_WINDOW_QUIT = SAFE_RANGE + 110,
     VIM_WINDOW_CLOSE_OTHERS = SAFE_RANGE + 111,
+    VIM_NORMAL_MODE_ENTER = SAFE_RANGE + 112,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -101,6 +102,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 SEND_STRING(SS_TAP(X_ESC)); // make sure we are in NORMAL mode
                 SEND_STRING("vi<"); // enter VISUAL mode, select everything between the next quotes
+            }
+            break;
+        case VIM_NORMAL_MODE_ENTER:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_ESC)); // make sure we are in NORMAL mode
+                SEND_STRING("i"); // go in insert mode
+                SEND_STRING(SS_TAP(X_ENTER)) // press Enter
+                SEND_STRING(SS_TAP(X_ESC)); // go back to NORMAL mode
             }
             break;
     }
@@ -195,10 +204,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_DEL,             KC_MUTE,
-        KC_GRAVE,   TD(TD_ONE_EXCLAMATION),     TD(TD_TWO_AT),     TD(TD_THREE_HASH),     TD(TD_FOUR_DOLLAR),     TD(TD_FIVE_PERCENT),     TD(TD_SIX_CARET),     TD(TD_SEVEN_AMPERSAND),     TD(TD_EIGHT_ASTERISK),     TD(TD_NINE_OPENPAR),     TD(TD_ZERO_CLOSEPAR),     TD(TD_MINUS_UNDERSCORE),  TD(TD_PLUS_EQUAL),   KC_BACKSPACE,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     TD(TD_LBRC_LCBR),  TD(TD_RBRC_RCBR),  TD(TD_BACKSLASH_PIPE),            KC_PGDN,
-        LT(MOUSE, KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     TD(TD_SEMICOLON_COLON),  TD(TD_QUOTE_DQUOTE),            KC_ENT,             KC_HOME,
-        SC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     TD(TD_COMMA_LT),  TD(TD_DOT_GT),   TD(TD_SLASH_QM),            SC_RSPC,  KC_UP,
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_PLUS,   KC_BACKSPACE,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P, KC_LBRC, KC_RBRC, KC_BACKSLASH,            KC_PGDN,
+        LT(MOUSE, KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L, KC_SCLN, KC_QUOTE,            KC_ENT,             KC_HOME,
+        SC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M, KC_COMMA, KC_DOT, KC_SLASH,            SC_RSPC,  KC_UP,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 MO(ACCENTS),MO(MAC_FN),MO(VIM),  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     /*
@@ -318,7 +327,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         │       ││ Win Q ││       ││       ││       ││       ││       ││       ││       ││ Win O ││       ││ SEL [ ││ SEL { ││       ││       │
         └───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘
         ┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌────────────────┐┌───────┐
-        │       ││       ││       ││       ││       ││       ││ Win H ││       ││       ││       ││ SEL " ││ SEL ' ││                ││       │
+        │       ││       ││       ││       ││       ││       ││ Win H ││       ││       ││       ││ SEL " ││ SEL ' ││ Enter (Normal) ││       │
         └───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└───────┘└────────────────┘└───────┘
         ┌────────────────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌───────┐┌────────────────┐
         │                ││       ││       ││       ││ Win V ││       ││       ││       ││ SEL < ││       ││       ││       ││                │
@@ -332,7 +341,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            RGB_TOG,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SEL_PARENTHESES,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
         XXXXXXX,  VIM_WINDOW_QUIT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_WINDOW_CLOSE_OTHERS,  XXXXXXX,  VIM_SEL_BRACKETS,  VIM_SEL_CURLY_BRACKETS,  XXXXXXX,            XXXXXXX,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SPLIT_WINDOW_HORIZONTALLY,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SEL_DOUBLE_QUOTES, VIM_SEL_QUOTE,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SPLIT_WINDOW_HORIZONTALLY,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SEL_DOUBLE_QUOTES, VIM_SEL_QUOTE,  VIM_NORMAL_MODE_ENTER,  XXXXXXX,
         KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SPLIT_WINDOW_VERTICALLY,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_SEL_ANGLE_BRACKETS,  XXXXXXX,  XXXXXXX,  KC_RSFT,  XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,                               KC_SPC,                                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  VIM_MOVE_WINDOW_SWITCH)
 
